1f19a73111824a9bb97bafc287b690066408bab0
yazmincosmeescobedo <yazmincosmeescobedo@gmail.com>
2022-07-05 12:51:22 -0400

    commit

--- challenges1.js
+++ challenges1.js
@@ -1,1 +1,9 @@
+//VERY EASY// 
+
+var firstNum = 6;
+var secondNum = 2; 
+
+var result = firstNum - secondNum; 
+
+console.log('The difference between ${firstNum} and ${secondNum} is ${result}'); 
 
