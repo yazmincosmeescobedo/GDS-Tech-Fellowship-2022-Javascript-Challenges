f13d8ac9919fa3440d0365635fdc00b1226a2098
yazmincosmeescobedo <yazmincosmeescobedo@gmail.com>
2022-07-05 16:19:50 -0400

    commit

--- challenges1.js
+++ challenges1.js
@@ -1,58 +1,58 @@
 // -- VERY EASY ---// 
 
-// var firstNum = 6;
-// var secondNum = 2; 
+var firstNum = 6;
+var secondNum = 2; 
 
-// var result = firstNum - secondNum; 
+var result = firstNum - secondNum; 
 
-// console.log(`The difference between ${firstNum} and ${secondNum} is ${result}`)
+console.log(`The difference between ${firstNum} and ${secondNum} is ${result}`)
 
 
 
 
 // --- EASY ---// 
 
-// var name1 = "Thomas"; 
-// var name2 = "John"; 
-// var length; 
+var name1 = "Thomas"; 
+var name2 = "John"; 
+var length; 
 
-// if(name1.length > name2.length){
-//     var longerName = name1;
-//     var shorterName = name2; 
-//     length = name1.length - name2.length; 
-// }
-// else{
-//     var longerName = name2;
-//     var shorterName = name1; 
-//     length = name2.length - name1.length; 
-// }
+if(name1.length > name2.length){
+    var longerName = name1;
+    var shorterName = name2; 
+    length = name1.length - name2.length; 
+}
+else{
+    var longerName = name2;
+    var shorterName = name1; 
+    length = name2.length - name1.length; 
+}
 
-// console.log(`The name ${longerName} is longer than ${shorterName} by ${length}`)
+console.log(`The name ${longerName} is longer than ${shorterName} by ${length}`)
 
 
 
 
 // --- MEDIUM --- // 
 
-// var sentence = prompt("Say spmething: "); 
+var sentence = prompt("Say spmething: "); 
 
 //Could add a third option for blended sentences or num inputs
-// if(sentence == sentence.toUpperCase()){
-//     var result = "SHOUTING"; 
-// }
-// else if(sentence == sentence.toLowerCase()){ 
-//     var result = "whispering";
-// }
+if(sentence == sentence.toUpperCase()){
+    var result = "SHOUTING"; 
+}
+else if(sentence == sentence.toLowerCase()){ 
+    var result = "whispering";
+}
 
-// console.log(`It appears you are ${result}`)
+console.log(`It appears you are ${result}`)
 
 
 
 
 // --- HARD --- // 
 
-var num1 = 8;
-var num2 = 2; 
+var num1; 
+var num2; 
 var result; 
 
 // This code can be made more dry but result variable 
